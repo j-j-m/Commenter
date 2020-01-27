919600a0d4918b3d3861be315f689dc29fe7cc0a
Jacob Martin <martinjaco@gmail.com>
2020-01-27 09:17:41 -0500

    add host app

---
+++ CommenterHost/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CommenterHost
+//
+//  Created by Jacob Martin on 1/26/20.
+//  Copyright © 2020 Jacob Martin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
